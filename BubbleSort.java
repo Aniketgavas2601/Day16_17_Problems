130a8cc9d7ec7067300a22670a6636d9048e260b
Experiment2601 <aniketgavas18@gmail.com>
2022-10-31 16:34:42 +0530

    BubbleSort

---
+++ BubbleSort.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.day16_17_problems;
+
+public class BubbleSort {
+    public static void printArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {9,4,2,10,7};
+
+        //Bubble sort
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        printArray(arr);
+    }
+}
